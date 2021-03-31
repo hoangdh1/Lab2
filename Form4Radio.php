d2fcd9b689a466cd253035bc0c4e01ae5eb2470f
hoangdh1 <66484333+hoangdh1@users.noreply.github.com>
2021-03-31 14:34:08 +0700

    lab2.7

--- Form4Radio.php
+++ Form4Radio.php
@@ -7,8 +7,8 @@
   <body>
     <font size=5>Thank You: Got Your Input.</font>
     <?php
-      $email = $_POST["email"];
-      $contact = $_POST["contact"];
+      $email = $_GET["email"];
+      $contact = $_GET["contact"];
       print ("<br>Your email address is $email");
       print ("<br> Contact preference is $contact");
      ?>
